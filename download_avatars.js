3ac88275f7c5df7dd40d0c5b23c7cd1418568af9
Mustafa Tariq <mustafatariq04@gmail.com>
2019-06-03 23:15:54 +0000

    step 6 completed - updated access token

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,33 @@
 var request = require('request');
+var GITHUB_TOKEN = require('./secrets.js');
+
 
 console.log('Welcome to the GitHub Avatar Downloader!');
+
+function getRepoContributors(repoOwner, repoName, cb) {
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'request',
+      }
+  };
+
+
+  request.get (options, function(err, res, body) {
+    if(err) {
+      console.log("error occurred :", err);
+    } else if (res.statusCode === 200) {
+      console.log(body);
+      var data = JSON.parse(body);
+      cb(data);
+    }
+    console.log("DONE with everything")
+  });
+};
+
+
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
